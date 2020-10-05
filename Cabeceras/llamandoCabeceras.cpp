590a61ccd56a48fa50687e7daf064035ec5f37dc
Gil <47683202+Gil-Campos@users.noreply.github.com>
2020-10-05 15:09:59 -0600

    Adding small example on how to call and create header files

---
+++ Cabeceras/llamandoCabeceras.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "cabeceraTest.h"
+
+using namespace std;
+
+int main() {
+
+    //Vamos a imprimir en consola "Hello World desde la cabecera test"
+    helloWorld();
+}
