662377901797e99012cb5e2978039e9c78bdd822
Gil <47683202+Gil-Campos@users.noreply.github.com>
2020-10-20 14:21:38 -0600

    Add a example of listas desde cero but this time i'm not using pointers, i just use a globlal variable which shortens code.

--- Listas Enlazadas Simples/listasDesdeCero.cpp
+++ Listas Enlazadas Simples/listasDesdeCero.cpp
@@ -1,79 +1,117 @@
-/*
-Para los siguientes ejercicios pueden realizarse ocupando la librería o realizando listas
-simples enlazadas desde 0.
-
-1) Realice un programa que muestre un menú que permita administrar una lista simple con
-datos acerca de personas. Deberá desplegar un menú.
-
-Para cada persona se deberá considerar que posee los siguientes datos: carnet, nombre,
-apellido, edad, número de teléfono y correo electrónico.
-*/
-
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-// Definicion del registro Libro
-struct persona 
-{
-    int carnet;
-    string nombre;
-    string apellido;
-    int edad;
-    int telefono;
-    string email;
+// Definicion del registro Libro ----------------
+
+enum generos{ciencia_ficcion,aventura,policial,romance,misterio};
+
+struct Libro{
+    int isbn;
+    string titulo;
+    string autor;
+    generos genero;
+    bool espanol;
 };
 
-typedef struct persona Persona;
+// Funciones propias de los Libros ----------------
 
-typedef Persona T;
-const T noValido = {-1, " ", " ", -1, -1, " "};
+Libro solicitarDato(){
+    Libro p;
+    
+    //---Solicitar ISBN, Titulo y Autor---
+    cout << "ISBN: "; cin >> p.isbn; cin.ignore();
+    cout << "Titulo: "; getline(cin, p.titulo);
+    cout << "Autor: "; getline(cin, p.autor);
+    
+    //---Solicitar genero del libro---
+    bool continuar = true;
+    do{
+        char opcion = 0;
+        cout << "Genero (c-ciencia|a-aventura|p-policial|r-romance|m-misterio): ";
+        cin >> opcion;
+        switch(opcion){
+            case 'c': p.genero = ciencia_ficcion; continuar = false; break;
+            case 'a': p.genero = aventura; continuar = false; break;
+            case 'p': p.genero = policial; continuar = false; break;
+            case 'r': p.genero = romance; continuar = false; break;
+            case 'm': p.genero = misterio; continuar = false; break;
+            default: cout << "Opcion erronea!" << endl; break;
+        }
+    }while(continuar);
+    
+    //---Solicitar idioma del libro---
+    continuar = true;
+    do{
+        char letra = 0;
+        cout << "Espanol (s/n): ";
+        cin >> letra;
+        switch(letra){
+            case 's': 
+            case 'S': p.espanol = true; continuar = false; break;
+            case 'n': 
+            case 'N': p.espanol = false; continuar = false; break;
+            default: cout << "Opcion erronea!" << endl; break;
+        }
+    }while(continuar);
+    
+    //---Devolver libro ya poblado---
+    return p;
+}
 
-// Definicion de la lista simplemente enlazada
-struct Nodo 
-{
+void mostrarDato(Libro p){
+    cout << "ISBN: " << p.isbn << endl;
+    cout << "Titulo: " << p.titulo << endl;
+    cout << "Autor: " << p.autor << endl;
+    
+    cout << "Genero: ";
+    switch(p.genero){
+        case ciencia_ficcion: cout << "Ciencia ficcion" << endl; break;
+        case aventura: cout << "Aventura" << endl; break;
+        case policial: cout << "Policial" << endl; break;
+        case romance: cout << "Romance" << endl; break;
+        case misterio: cout << "Misterio" << endl; break;
+    }
+    
+    cout << "Idioma: " << (p.espanol ? "Espanol" : "Otro") << "\n\n";
+}
+
+bool comparar(Libro a, Libro b){
+    return (a.isbn == b.isbn) && (a.titulo==b.titulo) &&
+        (a.autor==b.autor) && (a.genero==b.genero) && 
+        (a.espanol==b.espanol);
+}
+
+// Definicion de la lista simplemente enlazada ----------------
+typedef Libro T;
+const T noValido = {-1,{""},{""},misterio,false};
+
+struct Nodo{
     T dato;
     struct Nodo *sig;
 };
+Nodo *pInicio;
 
-typedef struct Nodo *Lista;
+// Prototipos de funciones de la lista ----------------
+int cantidadDatos();
+void insertarInicio(T p);
+void insertarFinal(T p);
+void insertarDespuesDe(T p);
+void insertarAntesDe(T p);
+void agregar();
+void mostrar();
+void eliminar();
+void buscar();
+void invertir(Nodo** nodoInicio);
+void vaciar(Nodo** nodoInicio);
+int contarOcurrencias();
+T acceder();
 
-//Prototipo de funciones
-void crearLista(Lista *p);
-bool empty(Lista *p);
-void insertarInicio(Lista *p, T x);
-void insertarFinal(Lista *p, T x);
-void insertarDespuesDe(Lista *p, T x);
-void insertarAntesDe(Lista *p, T x);
-void mostrarDatos(Persona p);
-void mostrar(Lista *p);
-void agregar(Lista *p);
-void eliminar(Lista *p);
-void buscar(Lista *p);
-void invertir(Lista* p);
-void vaciar(Lista *p);
-int contarOcurrencias(Lista *p);
-int cantidadDatos(Lista *p);
-T acceder(Lista *p);
-int solicitarCarnet();
-Persona solicitarDatos();
-void driver();
-
-int main()
-{
-    driver();
-
-    return 0;
-}
-
-//Implementacion de funciones
-void driver()
-{
-    Lista unaLista;
-
-    crearLista(&unaLista);
-
+// Programa principal ----------------
+int main(){
+    cout << "Inicializando..." << endl;
+    pInicio = NULL;
+    
     bool continuar = true;
     do{
         int opcion = 0;
@@ -85,202 +123,27 @@ void driver()
         cout << "\tOpcion elegida: ";
         cin >> opcion;
         switch(opcion){
-            case 1: agregar(&unaLista); break;
-            case 2: mostrar(&unaLista); break;
-            case 3: eliminar(&unaLista); break;
-            case 4: buscar(&unaLista); break;
-            case 5: invertir(&unaLista); break;
-            case 6: vaciar(&unaLista); break;
-            case 7: cout << "Hay " << cantidadDatos(&unaLista) << " datos.\n"; break;
-            case 8: cout << "Esta " << contarOcurrencias(&unaLista) << " veces.\n"; break;
-            case 9: acceder(&unaLista); break;
+            case 1: agregar(); break;
+            case 2: mostrar(); break;
+            case 3: eliminar(); break;
+            case 4: buscar(); break;
+            case 5: invertir(&pInicio); break;
+            case 6: vaciar(&pInicio); break;
+            case 7: cout << "Hay " << cantidadDatos() << " datos.\n"; break;
+            case 8: cout << "Esta " << contarOcurrencias() << " veces.\n"; break;
+            case 9: acceder(); break;
             case 10: continuar = false; break;
             default: cout << "Opcion erronea!" << endl; break;
         }
     }while(continuar);
+
+    return 0;
 }
 
-int solicitarCarnet() 
-{
-    cout << endl;
-    int num = 0;
-    cout << "Introduce el carnet del estudiante: ";
-    cin >> num;
-    cout << endl;
+// Implementacion de funciones de la lista ----------------
 
-    return num;
-}
-
-Persona solicitarDatos() 
-{
-    Persona input;
-    cout << "Inicializando..." << endl;
-
-    cout << endl;
-    cout << "Ingresa el nombre: ";
-    cin.ignore();
-    getline(cin, input.nombre);
-    cout << "Ingresa el apellido: ";
-    getline(cin, input.apellido);
-    cout << "Ingresa email: ";
-    getline(cin, input.email);
-    cout << "Ingresa el carnet: ";
-    cin >> input.carnet;
-    cout << "Ingresa el edad: ";
-    cin >> input.edad;
-    cout << "Ingresa el telefono: ";
-    cin >> input.telefono;
-
-    return input;
-}
-
-bool comparar(Persona a, int b)
-{
-    return a.carnet == b ? true : false;
-}
-
-void crearLista(Lista *p)
-{
-    *p = NULL;
-}
-
-bool empty(Lista *p)
-{
-    return *p == NULL;
-}
-
-void insertarInicio(Lista *p, T x) 
-{
-    Nodo *nuevo = new Nodo;
-    nuevo->dato = x;
-    nuevo->sig = *p;
-
-    *p = nuevo;
-}
-
-void insertarFinal(Lista *p, T x) 
-{
-    Nodo *nuevo = new Nodo;
-    nuevo->dato = x;
-    nuevo->sig = NULL;    
-
-    if (p == NULL)
-    {
-        *p = nuevo;
-    }
-    else
-    {
-        Nodo *unNodo = *p;
-        Nodo *aux = NULL;
-
-        while (unNodo != NULL)
-        {
-            aux = unNodo;
-            unNodo = unNodo->sig;
-        }
-        aux->sig = nuevo;
-    }
-}
-
-void insertarDespuesDe(Lista *p, T x)
-{
-    cout << endl;
-    cout << "Dato de referencia: ";
-    int ref = solicitarCarnet();
-
-    Nodo *s = *p;
-
-    while (s != NULL && !comparar(s->dato, ref))
-    {
-        s = s->sig;
-    }
-    
-    if (s == NULL)
-    {   
-        cout << endl;
-        cout << "Dato de referencia NO existe" << endl;
-        cout << endl;
-        return;
-    } 
-    else
-    {
-        Nodo *nuevo = new Nodo;
-        nuevo->dato = x;
-        nuevo->sig = s->sig;
-
-        s->sig = nuevo;
-        cout << endl;
-        cout << "Dato insertado con exito" << endl;
-        cout << endl;
-    } 
-}
-
-void insertarAntesDe(Lista *p, T x)
-{
-    cout << "Dato de referencia: ";
-    int ref = solicitarCarnet();
-
-    Nodo *s = *p, *q = NULL;
-
-    while (s != NULL && !comparar(s->dato, ref))
-    {
-        q = s;
-        s = s->sig;
-    }
-
-    if (s == NULL)
-    {
-        cout << endl;
-        cout << "Dato de referencia NO existe" << endl;
-        cout << endl;
-        return;
-    } 
-
-    Nodo *nuevo = new Nodo;
-    nuevo->dato = x;
-    nuevo->sig = s;
-
-    if (q == NULL)
-    {
-        *p = nuevo;
-    } 
-    else 
-    {
-        q->sig = nuevo;
-        cout << endl;
-        cout << "Dato insertado con exito" << endl;
-        cout << endl;
-    }
-    
-}
-
-void agregar(Lista *p)
-{
-    T datos = solicitarDatos();
-
-    bool continuar = true;
-    do
-    {
-        int opcion = 0;
-        cout << "\t1) Al principio\n";
-        cout << "\t2) Al final\n";
-        cout << "\t3) Despues de\n";
-        cout << "\t4) Antes de\n";
-        cout << "\tOpcion elegida: ";
-        cin >> opcion;
-        switch(opcion){
-            case 1: insertarInicio(p, datos); continuar = false; break;
-            case 2: insertarFinal(p, datos); continuar = false; break;
-            case 3: insertarDespuesDe(p, datos); continuar = false; break;
-            case 4: insertarAntesDe(p, datos); continuar = false; break;
-            default: cout << "Opcion erronea!" << endl; break;
-        }
-    } while(continuar);
-    
-}
-
-int cantidadDatos(Lista *p) {
-    Nodo *s = *p;
+int cantidadDatos() {
+    Nodo *s = pInicio;
 
     int contador = 0;
     while (s != NULL) {
@@ -291,152 +154,201 @@ int cantidadDatos(Lista *p) {
     return contador;
 }
 
-void mostrar(Lista *p)
-{
-    Nodo *s = *p;
-
-    while (s != NULL) {
-        mostrarDatos(s->dato);
-        s = s->sig;
-    }   
-}
-
-void mostrarDatos(Persona p)
-{
-    cout << endl;
-
-    cout << "Nombre: " << p.nombre << endl;
-    cout << "Apellido: " << p.apellido << endl;
-    cout << "Edad: " << p.edad << endl;
-    cout << "Carnet: " << p.carnet << endl;
-    cout << "Email: " << p.email << endl;
-    cout << "Telefono: " << p.telefono << endl;
-    cout << endl;
-
-}
-
-void eliminar(Lista *p)
-{
-    cout << endl;
-    cout << "Dato a eliminar" << endl;
-    int  ref = solicitarCarnet();
-
-    Nodo *q = *p, *s = NULL;
-
-    while (q != NULL && !comparar(q->dato, ref))
-    {
-        s = q;
-        q = q->sig;
-    }
+void insertarInicio(T p) {
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = p;
+    nuevo->sig = pInicio;
     
-    if (q == NULL)
-    {
-        cout << endl;
-        cout << "Dato a borrar NO existe" << endl;
-        cout << endl;
+    pInicio = nuevo;
+}
+
+void insertarFinal(T p) {
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = p;
+    nuevo->sig = NULL;
+    
+    if (pInicio == NULL) {
+        pInicio = nuevo;
+    } else {
+        Nodo *p = pInicio;
+        Nodo *q = NULL;
+        while (p != NULL) {
+            q = p;
+            p = p->sig;
+        }
+        q->sig = nuevo;
+    }
+}
+
+void insertarDespuesDe(T p) {
+    cout << "Dato de referencia: ";
+    T ref = solicitarDato();
+    
+    Nodo *s = pInicio;
+    
+    while(s != NULL && !comparar(s->dato, ref))
+        s = s->sig;
+    if(s == NULL){
+        cout << "Dato de referencia NO existe" << endl;
         return;
     }
     
-    if(s == NULL)
-    {
-        *p = q->sig;
-    }
-    else 
-    {
-        s->sig = q->sig;
-    }
-
-    delete(q);
-    cout << endl;
-    cout << "Dato borrado!" << endl;
-    cout << endl;
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = p;
+    nuevo->sig = s->sig;
+    
+    s->sig = nuevo;
+    cout << "Dato insertado con exito" << endl;
 }
 
-void buscar(Lista *p)
-{
-    cout << "Dato a buscar";
-    int  ref = solicitarCarnet();
-
-    Nodo *s = *p;
-
-    while (s != NULL && !comparar(s->dato, ref))
-    {
+void insertarAntesDe(T p) {
+    cout << "Dato de referencia: ";
+    T ref = solicitarDato();
+    
+    Nodo *s = pInicio, *q = NULL;
+    
+    while(s != NULL && !comparar(s->dato, ref)){
+        q = s;
         s = s->sig;
     }
-
-    if (s != NULL)
-    {
-        cout << endl;
-        cout << "El dato SI se encuentra" << endl;
-        cout << endl;
+    if(s == NULL){
+        cout << "Dato de referencia NO existe" << endl;
+        return;
     }
-    else
-    {
-        cout << endl;
-        cout << "El dato NO se encuentra" << endl;
-        cout << endl;
-    }
-}
-
-void invertir(Lista* p)
-{
-    Nodo *resultante = NULL;
-    Nodo *actual = *p;
-    Nodo *siguiente;
-
-    while(actual != nullptr){
-        siguiente = actual->sig;
-
-        actual->sig = resultante;
-        resultante = actual;
-        actual = siguiente;
-    }
-    *p = resultante;
-
-    cout << endl << "Lista invertida con exito" << endl;
-}
-
-void vaciar(Lista *p)
-{
-    Nodo* actual = *p;
-    Nodo* siguiente;
     
-    while(siguiente != NULL){
-        siguiente = actual->sig;
-        delete(actual);
-        actual = siguiente;
-    }
-
-    *p = NULL;
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = p;
+    nuevo->sig = s;
+    
+    if(q == NULL)
+        pInicio = nuevo;
+    else
+        q->sig = nuevo;
+    cout << "Dato insertado con exito" << endl;
 }
 
-int contarOcurrencias(Lista *p)
-{
-    cout << "Dato de referencia: ";
-    int ref = solicitarCarnet();
+void agregar(){
+    T p = solicitarDato();
+    bool continuar = true;
+    do{
+        int opcion = 0;
+        cout << "\t1) Al principio\n";
+        cout << "\t2) Al final\n";
+        cout << "\t3) Despues de\n";
+        cout << "\t4) Antes de\n";
+        cout << "\tOpcion elegida: ";
+        cin >> opcion;
+        switch(opcion){
+            case 1: insertarInicio(p); continuar = false; break;
+            case 2: insertarFinal(p); continuar = false; break;
+            case 3: insertarDespuesDe(p); continuar = false; break;
+            case 4: insertarAntesDe(p); continuar = false; break;
+            default: cout << "Opcion erronea!" << endl; break;
+        }
+    }while(continuar);
+}
 
-    Nodo* actual = *p;
+void mostrar() {
+    Nodo *s = pInicio;
+
+    while (s != NULL) {
+        mostrarDato(s->dato);
+        s = s->sig;
+    }
+}
+
+void eliminar(){
+    cout << "Dato a eliminar: ";
+    T ref = solicitarDato();
+    
+    Nodo *p = pInicio, *q = NULL;
+    
+    while(p != NULL && !comparar(p->dato, ref)){
+        q = p;
+        p = p->sig;
+    }
+    if(p == NULL){
+        cout << "Dato a borrar NO existe" << endl;
+        return;
+    }
+    if(q == NULL)
+        pInicio = p->sig;
+    else
+        q->sig = p->sig;
+    delete(p);
+    cout << "Dato borrado!" << endl;
+}
+
+void buscar() {
+    cout << "Dato a buscar: ";
+    T ref = solicitarDato();
+    
+    Nodo *s = pInicio;
+
+    while(s != NULL && !comparar(s->dato, ref))
+        s = s->sig;
+    
+    //Si s!=NULL entonces el elemento SI se encuentra
+    //Si s==NULL entonces el elemento NO se encuentra
+    if(s != NULL)
+        cout << "El dato SI se encuentra" << endl;
+    else
+        cout << "El dato NO se encuentra" << endl;
+}
+
+void invertir(Nodo** nodoInicio){
+    Nodo* result = NULL;
+    Nodo* current = *nodoInicio;
+    Nodo* next;
+    
+    while(current != NULL){
+        next = current->sig;
+        
+        current->sig = result;
+        result = current;
+        
+        current = next;
+    }
+    *nodoInicio = result;
+}
+
+void vaciar(Nodo** nodoInicio){
+    Nodo* current = *nodoInicio;
+    Nodo* next;
+    
+    while(current != NULL){
+        next = current->sig;
+        delete(current);
+        current = next;
+    }
+    *nodoInicio = NULL;
+}
+
+int contarOcurrencias(){
+    cout << "Dato de referencia: ";
+    T ref = solicitarDato();
+
+    Nodo* current = pInicio;
     int contador = 0;
     
-    while(actual != NULL){
-        if(comparar(actual->dato, ref)) contador++;
-        actual = actual->sig;
+    while(current != NULL){
+        if(comparar(current->dato, ref)) contador++;
+        current = current->sig;
     }
-
     return contador;
 }
 
-T acceder(Lista *p) {
+T acceder() {
     int indice = 0;
     cout << "Indice a acceder: ";
     cin >> indice; cin.ignore();
     
-    if(indice>=0 && indice<cantidadDatos(p)){
-        Nodo *s = *p;
+    if(indice>=0 && indice<cantidadDatos()){
+        Nodo *s = pInicio;
         for (int i = 0; i < indice; i++) {
             s = s->sig;
         }
-        mostrarDatos(s->dato);
+        mostrarDato(s->dato);
         return s->dato;
     }
     else{
