8feb9bdca7eedcd8e487871b533150573d9881de
Gil <47683202+Gil-Campos@users.noreply.github.com>
2020-11-07 16:49:10 -0600

    Agregue parte de encapsulacion de POO en C++

---
+++ Programacion Orientada a Objetos - C++/encapsulation/encapsulation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Encapsulation 
+{
+    private:
+    
+    //data hidden from the outside world
+    int x;
+
+    public:
+    
+    //function to set the value of variable x
+    void set(int a)
+    {
+        x = a;
+    }
+
+    //function to return the value of variable x
+    int get()
+    {
+        return x;
+    }
+};
+
+int main()
+{
+    Encapsulation obj;
+
+    obj.set(5);
+
+    cout << obj.get() << endl;
+
+    return 0;
+}
