b49850f65aa38c9766a5c3fd45b44a3f47add335
Gil <47683202+Gil-Campos@users.noreply.github.com>
2020-11-07 17:06:55 -0600

    Agregue abstraccion de POO en C++

---
+++ Programacion Orientada a Objetos - C++/abstraction/implementAbstraction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class ImplementAbstraction
+{
+    private:
+    int a, b;
+
+    public:
+    //Method to set values of private numbers
+    void set(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void display()
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+};
+
+int main()
+{
+    ImplementAbstraction obj;
+
+    obj.set(10, 20);
+    obj.display();
+    
+    return 0;
+}
