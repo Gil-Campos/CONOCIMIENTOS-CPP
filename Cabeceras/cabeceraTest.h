3f9cef0f20b4ec23457cf04700a0848d40748288
Gil <47683202+Gil-Campos@users.noreply.github.com>
2020-11-20 23:14:45 -0600

    Agregue ejemplo de pragma once al file de cabeceras

--- Cabeceras/cabeceraTest.h
+++ Cabeceras/cabeceraTest.h
@@ -2,6 +2,7 @@
 Esta es solo una pequeña prueba de como llamar archivos cabecera, la implementacion de archivos con mas funcionabilidad
 y robustos requieren una arquitectura mas complicada para que no tener problemas a la hora de ejecucion.
 */
+#pragma once
 
 #include <iostream>
 
